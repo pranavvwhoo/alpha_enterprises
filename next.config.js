b582ee517a92ba1ac241548c481736eb4b3b0b07
Pranav Vaghela <pranav.v@coreprecisionindia.com>
2026-08-19 12:43:13 +0530

    Bugs Solved

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: '/.well-known/apple-developer-merchantid-domain-association',
+				headers: [
+					{ key: 'Content-Type', value: 'text/plain' },
+					{ key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate' },
+				],
+			},
+		]
+	},
+}
 
 module.exports = nextConfig
